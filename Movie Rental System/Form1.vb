e57cfdddac338cb0d9ab7a2c3e866262a43d60c1
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2023-06-21 23:51:11 +0000

    Done with Summary Button

--- Movie Rental System/Form1.vb
+++ Movie Rental System/Form1.vb
@@ -33,4 +33,26 @@
         NewReleaseCheckBox.Checked = False
         AmountTextBox.Text = String.Empty
     End Sub
+
+    Private Sub OrderCompleteBtn_Click(sender As Object, e As EventArgs) Handles OrderCompleteBtn.Click
+        ' Confirm order completion with the user
+        Dim result = MessageBox.Show("Are you sure you want to complete the order?", "Order Complete", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = DialogResult.Yes Then
+            ' Increase the total customer count
+            totalCustomers += 1
+
+            ' Clear the form for a new customer
+            MovieTitleTextBox.Text = String.Empty
+            VHSRadioBtn.Checked = False
+            DVDRadioBtn.Checked = False
+            MemberCheckBox.Enabled = True
+            MemberCheckBox.Checked = False
+            ClearBtn.PerformClick()
+        End If
+    End Sub
+
+    Private Sub SummaryBtn_Click(sender As Object, e As EventArgs) Handles SummaryBtn.Click
+        ' Display the summary information
+        MessageBox.Show($"Total Customers: {totalCustomers}{Environment.NewLine}Total Rental Amount: GH¢{totalRentalAmount}", "Summary")
+    End Sub
 End Class
