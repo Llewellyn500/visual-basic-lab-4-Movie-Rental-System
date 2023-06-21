712de4cb182f34ff37323a6ff6f8cc6a813d1bf9
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2023-06-21 23:19:21 +0000

    Done with app design

--- Movie Rental System/Form1.Designer.vb
+++ Movie Rental System/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class RentalForm
+Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,16 +22,191 @@ Partial Class RentalForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        GroupBox1 = New GroupBox()
+        Label2 = New Label()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        GroupBox1.SuspendLayout()
         SuspendLayout()
         ' 
-        ' RentalForm
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(111, 17)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(278, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(111, 66)
+        TextBox2.Name = "TextBox2"
+        TextBox2.ReadOnly = True
+        TextBox2.Size = New Size(147, 23)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 20)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(68, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Movie Title:"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(404, 17)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(98, 76)
+        GroupBox1.TabIndex = 3
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Movie Format"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 69)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(90, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "Rental Amount:"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(16, 22)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(47, 19)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "VHS"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(16, 47)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(48, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "DVD"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(523, 39)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(71, 19)
+        CheckBox1.TabIndex = 5
+        CheckBox1.Text = "Member"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(523, 65)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(92, 19)
+        CheckBox2.TabIndex = 6
+        CheckBox2.Text = "New Release"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 126)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(138, 48)
+        Button1.TabIndex = 7
+        Button1.Text = "Calculate"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(170, 126)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(138, 48)
+        Button2.TabIndex = 8
+        Button2.Text = "Clear"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(329, 126)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(138, 48)
+        Button3.TabIndex = 9
+        Button3.Text = "Order Complete"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(146, 194)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(344, 54)
+        Button4.TabIndex = 10
+        Button4.Text = "Exit"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(477, 126)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(138, 48)
+        Button5.TabIndex = 11
+        Button5.Text = "Summary"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Name = "RentalForm"
+        ClientSize = New Size(643, 276)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Label2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
         Text = "Movie Rental System"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
 End Class
--- Movie Rental System/Form1.vb
+++ Movie Rental System/Form1.vb
@@ -1,3 +1,5 @@
-﻿Public Class RentalForm
+﻿Public Class Form1
+    Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 End Class
